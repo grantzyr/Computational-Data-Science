6c7e796bb5bed23fa9b7e2397ad6a9d8f79c1dfe
Grant Z <grantzyr@gmail.com>
2020-06-19 19:47:43 -0700

    add fix2

--- e5/reddit_weekends.py
+++ e5/reddit_weekends.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+from datetime import date
 
 OUTPUT_TEMPLATE = (
     "Initial (invalid) T-test p-value: {initial_ttest_p:.3g}\n"
@@ -31,6 +32,7 @@ def main():
 	norWeekdays = stats.normaltest(weekdays['comment_count']).pvalue
 	norWeekends = stats.normaltest(weekends['comment_count']).pvalue
 	
+	ttestPvalue = stats.ttest_ind(weekdays['comment_count'], weekends['comment_count']).pvalue
 	levPvalue = stats.levene(weekdays['comment_count'], weekends['comment_count']).pvalue
 	# print(lev_pvalue)
 	# 0.04378740989202803
@@ -46,19 +48,91 @@ def main():
 	# plt.hist(logWeekdays, alpha=0.5)
 	# plt.hist(logWeekends, alpha=0.5)
 	# plt.show()
+	# logNorWeekdays = stats.normaltest(logWeekdays).pvalue
+	# logNorWeekends = stats.normaltest(logWeekends).pvalue 
+	# logLevPvalue = stats.levene(logWeekdays, logWeekends).pvalue
+	# print(logNorWeekdays)
+	# print(logNorWeekends)
+	# print(logLevPvalue)
 
 	# np.exp
-	expWeekdays = np.exp(weekdays['comment_count'])
-	expWeekends = np.exp(weekends['comment_count'])
+	# expWeekdays = np.exp(weekdays['comment_count'])
+	# expWeekends = np.exp(weekends['comment_count'])
 	# plt.hist(expWeekdays, alpha=0.5)
 	# plt.hist(expWeekends, alpha=0.5)
 	# plt.show()
-	print(expWeekdays)
-	print(expWeekends)
+	# print(expWeekdays)
+	# print(expWeekends)
+	# expNorWeekdays = stats.normaltest(expWeekdays).pvalue
+	# expNorWeekends = stats.normaltest(expWeekends).pvalue
+	# expLevPvalue = stats.levene(expWeekdays, expWeekends).pvalue
+	# print(expNorWeekdays)
+	# print(expNorWeekends)
+	# print(expLevPvalue)
+
+	# np.sqrt
+	sqrtWeekdays = np.sqrt(weekdays['comment_count'])
+	sqrtWeekends = np.sqrt(weekends['comment_count'])
+	# plt.hist(sqrtWeekdays, alpha=0.5)
+	# plt.hist(sqrtWeekends, alpha=0.5)
+	# plt.show()
+	sqrtNorWeekdays = stats.normaltest(sqrtWeekdays).pvalue 
+	sqrtNorWeekends = stats.normaltest(sqrtWeekends).pvalue  
+	sqrtLevPvalue = stats.levene(sqrtWeekdays, sqrtWeekends).pvalue 
+
+
+	# counts**2
+	# e2Weekdays = (weekdays['comment_count']) ** 2
+	# e2Weekends = (weekends['comment_count']) ** 2
+	# plt.hist(e2Weekdays, alpha=0.5)
+	# plt.hist(e2Weekends, alpha=0.5)
+	# plt.show()
+	# e2NorWeekdays = stats.normaltest(e2Weekdays).pvalue 
+	# e2NorWeekends = stats.normaltest(e2Weekends).pvalue 
+	# e2LevPvalue = stats.levene(e2Weekdays, e2Weekends).pvalue 
+	# print(e2NorWeekdays)
+	# print(e2NorWeekends)
+	# print(e2LevPvalue)
+
+	# np.sqrt is the best
+
+	# fix2: the central limit theorem might save us.
+	weekOfYearWeekdays = weekdays['date'].apply(date.isocalendar).apply(pd.Series)
+	weekOfYearWeekends = weekends['date'].apply(date.isocalendar).apply(pd.Series)
+	weekdays = weekdays.join(weekOfYearWeekdays).rename(columns = {0:'year', 1:'week', 2:'day'})
+	weekends = weekends.join(weekOfYearWeekends).rename(columns = {0:'year', 1:'week', 2:'day'})
+	weekdaysMean = weekdays.groupby(['year', 'week'])['comment_count'].mean().reset_index() 
+	weekendsMean = weekends.groupby(['year', 'week'])['comment_count'].mean().reset_index()
+
+
+	norWeekdaysMean = stats.normaltest(weekdaysMean['comment_count']).pvalue 
+	norWeekendsMean = stats.normaltest(weekendsMean['comment_count']).pvalue
+	levMean = stats.levene(weekdaysMean['comment_count'],weekendsMean['comment_count']).pvalue
+	ttestMean = stats.ttest_ind(weekdaysMean['comment_count'],weekendsMean['comment_count']).pvalue
+
+
+
+	# fix3: a non-parametric test might save us
+	
+	
 
 
 
 
+	print(OUTPUT_TEMPLATE.format(
+	        initial_ttest_p=ttestPvalue,
+	        initial_weekday_normality_p=norWeekdays,
+	        initial_weekend_normality_p=norWeekends,
+	        initial_levene_p=levPvalue,
+	        transformed_weekday_normality_p=sqrtNorWeekdays,
+	        transformed_weekend_normality_p=sqrtNorWeekends,
+	        transformed_levene_p=sqrtLevPvalue,
+	        weekly_weekday_normality_p=norWeekdaysMean,
+	        weekly_weekend_normality_p=norWeekendsMean,
+	        weekly_levene_p=levMean,
+	        weekly_ttest_p=ttestMean,
+	        utest_p=0,
+	    ))
 if __name__ == '__main__':
 	main()
 
